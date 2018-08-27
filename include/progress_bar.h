78355607af0a6d8d45180182b999d6270f67a42d
Benjamin Kiesl <kiesl@kr.tuwien.ac.at>
2018-08-27 18:11:14 +0200

    more comments.

--- include/progress_bar.h
+++ include/progress_bar.h
@@ -7,6 +7,13 @@ namespace drat2er {
 // process to the standard output.
 class ProgressBar {
   public:
+    // By setting the overall number of items to a given value, you
+    // can use the PrintProgress() overload that takes as input the
+    // 'number_of_items_processed'. If, for instance, the overall number
+    // of items is set to 10, and then PrintProgress() is called with 4,
+    // then the progress bar displays a progress of 40%.
+    // The parameter 'width' indicates the width (in number of symbols) of
+    // the progress bar as displayed in the standard output.
     ProgressBar(int overall_number_of_items=1, int width=22);
     ~ProgressBar();
     int GetOverallNumberOfItems(int overall_number_of_items);
